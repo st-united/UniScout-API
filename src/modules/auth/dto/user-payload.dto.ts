02608ee28861aa7adf1a95a3df5d13e99cedc646
Chua Yi Hang <2401519@sit.singaporetech.edu.sg>
2025-05-28 14:56:45 +0800

    Update user-payload.dto.ts

--- src/modules/auth/dto/user-payload.dto.ts
+++ src/modules/auth/dto/user-payload.dto.ts
@@ -1,5 +1,9 @@
+// src/modules/auth/dto/user-payload.dto.ts
+import { UserRole } from '../../../common/constants/enums'; // Ensure this path is correct
+
 export class UserPayloadDto {
   id: number;
   email: string;
   name: string;
+  role: UserRole; // <--- ADD THIS LINE
 }
